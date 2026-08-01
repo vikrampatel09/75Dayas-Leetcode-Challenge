13971ce7897619087056acf9c430eed05d609e7f
vikrampatel09 <167686008+vikrampatel09@users.noreply.github.com>
2026-08-01 17:00:13 +0530

    Time: 2 ms (72.96%), Space: 27.4 MB (6.12%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    bool checkPalindrome(string s, int i, int j) {
+        while(i < j) {
+            if(s[i] != s[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j) {
+
+            if(s[i] == s[j]) {
+                i++;
+                j--;
+            }
+            else {
+                bool caseOneAns = checkPalindrome(s, i + 1, j);
+                bool caseTwoAns = checkPalindrome(s, i, j - 1);
+
+                return caseOneAns || caseTwoAns;
+            }
+        }
+
+        return true;
+    }
+};
